4c50fbb1833b3d8c92d92223b04df3fa5dee793d
samuelluist <108497456+samuelluist@users.noreply.github.com>
2022-07-02 19:02:34 -0400

    Update inserts.sql

--- DML/inserts.sql
+++ DML/inserts.sql
@@ -27,3 +27,9 @@ WHERE id_categoria=3;
 --nuestro registro
 UPDATE public.categoria SET hist_premio_nt[1] = (hist_premio_nt[1].fecha_ini, '2022-12-31', hist_premio_nt[0].nombre, hist_premio_nt[0].cantidad_nom)::hist_premio
 WHERE id_categoria = 93;
+
+--INSERTAR PELICULA EN AUDIOVISUAL
+INSERT INTO public.audiovisual(
+	 titulo_espanol, titulo_original, sinopsis, fecha_estreno_cine, duracion_min, censura, ano_realizacion, genero_va,clas_audiovisual, total_nomi, total_postu, total_ganador)
+	VALUES ( 'Que paso ayer?', 'Hangover', '4 amigo, despedida de solteros, el que se iba a casar se pierde, caos', '2016-5-20', 160, 'C', 2021, '{"Comedia","Drama"}','largometraje', 0, 0, 0);
+	
