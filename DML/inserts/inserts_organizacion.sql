68c5c818c0cee1e82bef936b08072e424062d753
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-03 00:47:17 -0400

    Inserts agregando id

--- DML/inserts/inserts_organizacion.sql
+++ DML/inserts/inserts_organizacion.sql
@@ -1,17 +1,17 @@
 -------------- ORGANIZACIONES --------------
 
 INSERT INTO public.organizacion(
-	nombre, mision, donacion_nt)
-	VALUES ('African Film Festival', 'African Film Festival, Inc. es una organización cultural sin ánimo de lucro estadounidense que presenta un festival de cine anual y programas comunitarios durante todo el año', array[('1987', 35, 1257.12)::donacion]);
+	id_organizacion, nombre, mision, donacion_nt)
+	VALUES ( 1,'African Film Festival', 'African Film Festival, Inc. es una organización cultural sin ánimo de lucro estadounidense que presenta un festival de cine anual y programas comunitarios durante todo el año', array[('1987', 35, 1257.12)::donacion]);
 
 INSERT INTO public.organizacion(
-	nombre, mision, donacion_nt)
-	VALUES ('The Film Foundation', 'The Film Foundation es una organización estadounidense sin ánimo de lucro dedicada a la conservación de películas y a la exhibición de cine clásico y restaurado. Fue fundada por el director Martin Scorsese y otros destacados cineastas', array[('1988', 10, 157.12)::donacion, ('1989', 12, 1505.10)::donacion]);
+	id_organizacion, nombre, mision, donacion_nt)
+	VALUES ( 2,'The Film Foundation', 'The Film Foundation es una organización estadounidense sin ánimo de lucro dedicada a la conservación de películas y a la exhibición de cine clásico y restaurado. Fue fundada por el director Martin Scorsese y otros destacados cineastas', array[('1988', 10, 157.12)::donacion, ('1989', 12, 1505.10)::donacion]);
 
 INSERT INTO public.organizacion(
-	nombre, mision, donacion_nt)
-	VALUES ('Martin Luther King Jr. Center', 'The Martin Luther King Jr. Center for Nonviolent Social Change, comúnmente conocido como The King Center, es una organización no gubernamental sin fines de lucro en Atlanta, Estados Unidos.', array[('1985', 40, 15007.27)::donacion, ('1986', 24, 10505.10)::donacion]);
+	id_organizacion, nombre, mision, donacion_nt)
+	VALUES ( 3,'Martin Luther King Jr. Center', 'The Martin Luther King Jr. Center for Nonviolent Social Change, comúnmente conocido como The King Center, es una organización no gubernamental sin fines de lucro en Atlanta, Estados Unidos.', array[('1985', 40, 15007.27)::donacion, ('1986', 24, 10505.10)::donacion]);
 
 INSERT INTO public.organizacion(
-	nombre, mision, donacion_nt)
-	VALUES ('Children’s Defense Fund', 'The Martin Luther King Jr. Center for Nonviolent Social Change, comúnmente conocido como The King Center, es una organización no gubernamental sin fines de lucro en Atlanta, Estados Unidos.', array[('1985', 40, 15007.27)::donacion, ('1986', 24, 10505.10)::donacion]);
+	id_organizacion, nombre, mision, donacion_nt)
+	VALUES ( 4,'Children’s Defense Fund', 'Fundado en 1973 por Marian Wright Edelman, Childrens Defense Fund se enfoca en los derechos de los niños. Fue inspirado por el trabajo de organizaciones de derechos civiles. A través de programas y defensa, CDF trabaja para poner fin a los ciclos de pobreza, proteger a los niños del abuso y asegurarse de que todos los niños reciban una educación. Esta ONG tiene su sede en Washington, DC con oficinas en todo el país.', array[('1987', 21, 500007.27)::donacion, ('1988', 70, 150505.10)::donacion]);
