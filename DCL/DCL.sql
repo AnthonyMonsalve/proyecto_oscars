964ae1f6064fa4c73edbdd5767495514e2ae9cc7
Rayner Betancourt <raynerb3000@gmail.com>
2022-07-03 21:37:34 -0400

    Roles y privilegios completo, falta la parta para el permiso de ejecucion de programas almacenados para terminar

--- DCL/DCL.sql
+++ DCL/DCL.sql
@@ -2,46 +2,40 @@
     REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 
     -- INI VISITANTE 
-        CREATE ROLE user_oscar;
-        GRANT USAGE ON SCHEMA public TO user_oscar;
-        GRANT SELECT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO user_oscar;
-        CREATE USER visitante WITH PASSWORD '1234';
-        GRANT user_oscar TO visitante;
+        CREATE ROLE visitante LOGIN PASSWORD '1234';;
+        GRANT SELECT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO visitante;
     -- FIN VISITANTE
 
     -- INI ANALISTA DE REDES
-        CREATE ROLE analista_redes;
-        GRANT SELECT, INSERT ON critica TO analista_redes;
-        CREATE USER analista_ LOGIN PASSWORD '1234';
-        GRANT analista_redes TO analista;
+        CREATE ROLE analista WITH LOGIN PASSWORD '1234';
+        GRANT SELECT, INSERT ON critica TO analista;
     -- FIN ANALISTA DE REDES
     
     -- INI MIEMBRO
-        CREATE ROLE user_miembro;
-        GRANT SELECT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO user_miembro;
-        GRANT INSERT ON TABLE postuladas_p_pers, postulado_votos, votos TO user_miembro;
+        CREATE ROLE miembro WITH LOGIN PASSWORD '1234';
+        GRANT SELECT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO miembro;
+        GRANT INSERT ON TABLE postuladas_p_pers, postulado_votos, votos TO miembro;
         -- GRANT EXECUTE (LOS PROGRAMAS ALMACENADOS)
-        CREATE USER miembro LOGIN password '1234';
-        GRANT user_miembro TO miembro;
     -- FIN MIEMBRO
 
     -- INI DIRECTIVO
-        CREATE ROLE user_directivo;
-        GRANT SELECT, UPDATE, INSERT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO user_directivo;
-        CREATE USER directivo WITH PASSWORD '1234';
-        GRANT user_directivo TO directivo;
+        CREATE ROLE directivo WITH LOGIN PASSWORD '1234';
+        GRANT SELECT, UPDATE, INSERT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO directivo;
     -- FIN DIRECTIVO
 
     -- INI ADMINISTRADOR
-        CREATE ROLE administrador;
+        CREATE ROLE administrador WITH LOGIN PASSWORD '1234';
+        GRANT SELECT ON TABLE audiovisual, persona, rol, rol_pel_pers, gala, presentador, categoria, nominadas TO administrador;
+        -- GRANT 
         -- GRANT EXECUTE (LOS PROGRAMAS ALMACENADOS)
     -- FIN ADMINISTRADOR
 
     -- INI DESARROLLADOR
-        CREATE ROLE desarrollador;
+        CREATE ROLE desarrollador WITH CREATEDB LOGIN PASSWORD '1234';
     -- FIN DESARROLLADOR
 
     -- INI DBA
-        CREATE ROLE dba;
+        CREATE ROLE data_base_administrator WITH SUPERUSER LOGIN PASSWORD '1234';
     -- FIN DBA
+
 -- FIN SEGURIDAD
