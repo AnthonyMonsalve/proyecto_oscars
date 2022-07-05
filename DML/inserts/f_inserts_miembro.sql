3c14b951ce1bf587703ea38b8e5f7449f3bddc08
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-05 16:27:29 -0400

    MIEMBROS INSERTS

--- DML/inserts/f_inserts_miembro.sql
+++
@@ -1,9 +0,0 @@
--- INI MIEMBRO
-    INSERT INTO public.miembro(
-        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
-        VALUES (?, ?, ?, ?);
-
-    INSERT INTO public.miembro(
-        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
-        VALUES (?, ?, ?, ?);
--- FIN MIEMBRO
---
+++ DML/inserts/inserts_miembro.sql
@@ -0,0 +1,48 @@
+-- INI MIEMBRO
+
+-- 'Robert', 'Zemeckis'
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1980-05-05', 'no', 12545, NULL);
+
+-- 'Michael', 'Fox' 
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1979-11-09', 'no', 7826, NULL);
+
+-- 'John', 'Victor' MIEMBRO VITALICIO
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1955-05-07', 'si', 7025, NULL);
+
+-- 'James', 'Cameron'
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1959-10-15', 'no', 1665, '1990-11-05');
+
+-- 'Susan', 'Weaver' MIEMBRO VITALICIO
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1969-12-05', 'si', 1122, '1989-09-15');
+
+-- 'Robert', 'Wise'
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1939-01-03', 'no', 2665, NULL);
+
+-- 'Claire', 'Trevor'
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1931-05-28', 'no', 7211, NULL);
+
+-- 'James', 'Saburo'
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1939-09-09', 'no', 3548, NULL);
+
+-- 'Michael', 'Kamen' MIEMBRO VITALICIO
+    INSERT INTO public.miembro(
+        fecha_inicio, vitalicio, doc_identidad, fecha_fin)
+        VALUES ('1967-06-06', 'si', 74511, NULL);
+
+-- FIN MIEMBRO
