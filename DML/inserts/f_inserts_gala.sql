7b715d022cdd70c50679c7513e300995b043a1de
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-05 23:29:26 -0400

    INSERTS DE GALA Y CASOS DE USO DE VALIDAD_ORG

--- DML/inserts/f_inserts_gala.sql
+++ DML/inserts/f_inserts_gala.sql
@@ -1,9 +1,5 @@
 -- INI GALA
     INSERT INTO public.gala(
         ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
-        VALUES (?, ?, ?, ?, ?, ?);
-
-    INSERT INTO public.gala(
-        ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
-        VALUES (?, ?, ?, ?, ?, ?);
+        VALUES (1985, '1985-04-20', 'Eurobuilding', 50, 3548, 'Buena gala mi gente, estan invitados');
 -- FIN GALA
