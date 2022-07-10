e7a822b8005482c2926a1fe76b0a34c3406f30a9
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-10 15:26:11 -0400

    Revert "INSERTS POSTULADAS P PERSONAS"
    This reverts commit e8e4202c1486bbbab12167af9662673e30d8154c.

--- DML/inserts/inserts_gala.sql
+++ DML/inserts/inserts_gala.sql
@@ -2,8 +2,4 @@
     INSERT INTO public.gala(
         ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
         VALUES (1985, '1985-04-20', 'Eurobuilding', 50, 3548, 'Buena gala mi gente, estan invitados');
-
-    INSERT INTO public.gala(
-        ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
-        VALUES (1986, '1986-04-20', 'MGM', 51, 2015, 'Esta gala sera mejor que la anterior. Vendrán a cantar Nacho y Chino con el tema promocional "Mi niña fea". La gala empieza a partir de las 10:00 p.m, estan invitados');
 -- FIN GALA
--- DML/procedures.sql
+++ DML/procedures.sql
@@ -1,17 +1,17 @@
 
 -----------------------------------Procedimiento calculo de votos postulados e insercion en nominados------------------
 
-CREATE OR replace procedure gestion_postulados(
+create or replace procedure gestion_postulados(
    v_ano_oscar integer,
-   v_id_categoria integer
+	v_id_categoria integer
 )
-LANGUAGE plpgsql    
-AS $$
-DECLARE 
+language plpgsql    
+as $$
+Declare 
 	v_max_nom integer;
 	v_length_temp integer;
 	v_cantidad_max_nom integer;
-BEGIN
+begin
 
 	v_cantidad_max_nom=-1;
 	WITH list AS
@@ -20,10 +20,10 @@ BEGIN
 	  FROM   public.categoria
 	  WHERE  id_categoria= v_id_categoria )
 	SELECT   
-		   (row_result).cantidad_nom INTO v_cantidad_max_nom FROM list WHERE (row_result).fecha_fin IS NULL;
-	IF v_cantidad_max_nom IS NULL THEN 
+		   (row_result).cantidad_nom into v_cantidad_max_nom FROM list where (row_result).fecha_fin is null;
+	if v_cantidad_max_nom is null then 
 		RAISE EXCEPTION 'No existe ningun historico de este premio activo, por favor revisar el premio en la tabla categoria';
-	end IF;
+	end if;
 	
 	----Calculo de votos
 	CREATE TEMP TABLE tmp_votos (
@@ -35,21 +35,21 @@ BEGIN
 	);
 	
 	
-	INSERT INTO tmp_votos SELECT ano_oscar1, id_categoria1, id_postuladas_p_pers1, count(*)  FROM public.votos 
-	WHERE ano_oscar1=v_ano_oscar AND id_categoria1=v_id_categoria 
+	insert into tmp_votos select ano_oscar1, id_categoria1, id_postuladas_p_pers1, count(*)  from public.votos 
+	where ano_oscar1=v_ano_oscar and id_categoria1=v_id_categoria 
 	group by ano_oscar1, id_categoria1,id_postuladas_p_pers1
 	order by 4 DESC
 	limit v_max_nom;
 	
-	UPDATE tmp_votos SET ganador= 'no';
+	update tmp_votos set ganador= 'no';
 	
 	----Insertando votos en la tabla nominado
 	
 	INSERT INTO public.nominadas(
 	ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-	SELECT  ganador, id_postulado, ano_oscar, id_categoria FROM tmp_votos;
+	select  ganador, id_postulado, ano_oscar, id_categoria from tmp_votos;
 
-    COMMIT;
+    commit;
 end;$$
 
 -----Prueba
@@ -58,24 +58,24 @@ call gestion_postulados(2001,19);
 
 -----------------------------------Procedimiento calculo de votos nominados y actualizacion de ganador------------------
 
-CREATE OR replace procedure gestion_nominados(
+create or replace procedure gestion_nominados(
    v_ano_oscar integer,
 	v_id_categoria integer
 )
-LANGUAGE plpgsql    
-AS $$
-DECLARE 
+language plpgsql    
+as $$
+Declare 
 	v_id_ganador integer;
 	v_numero_votos integer;
-BEGIN
+begin
 
 	
 	
 	----Calculo de votos
 	
 	
-	SELECT id_nominada, count(*) INTO v_id_ganador, v_numero_votos FROM public.votos 
-	WHERE ano_oscar=v_ano_oscar AND id_categoria=v_id_categoria 
+	select id_nominada, count(*) into v_id_ganador, v_numero_votos from public.votos 
+	where ano_oscar=v_ano_oscar and id_categoria=v_id_categoria 
 	group by id_nominada
 	order by 2 DESC
 	limit 1;
@@ -83,9 +83,9 @@ BEGIN
 	
 	----Modificamos el parametro ganador
 	
-	UPDATE public.nominadas SET ganador= 'si' WHERE id_nominada= v_id_ganador; 
+	update public.nominadas set ganador= 'si' where id_nominada= v_id_ganador; 
 
-    COMMIT;
+    commit;
 end;$$
 
 -----Prueba
