fa8128a0682c051cdc51bccb7382ed23f749cb4c
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-10 15:34:04 -0400

    inserts postuladas p pers

--- DML/inserts/f_inserts_m_p.sql
+++
@@ -1,8 +0,0 @@
--- INI M_P
-    INSERT INTO public.m_p(
-        id_miembro, id_categoria)
-        VALUES (?, ?);
-    INSERT INTO public.m_p(
-        id_miembro, id_categoria)
-        VALUES (?, ?);
--- FIN M_P
