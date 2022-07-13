9693bd184289337d9849a0e00c1f71fbdb778485
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-13 17:57:46 -0400

    asignar presentadores de gala actual

--- DML/inserts/f_inserts_presentador.sql
+++
@@ -1,9 +0,0 @@
--- INI PRESENTADOR
-    INSERT INTO public.presentador(
-        id_gala, doc_identidad, id_categoria)
-        VALUES (?, ?, ?);
-
-    INSERT INTO public.presentador(
-        id_gala, doc_identidad, id_categoria)
-        VALUES (?, ?, ?);
--- FIN PRESENTADOR
