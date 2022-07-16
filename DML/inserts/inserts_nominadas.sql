4163633d53d02aee85cca89f0485e5ac8adb5784
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-07-16 14:31:13 -0400

    inserts de nomina

--- DML/inserts/inserts_nominadas.sql
+++ DML/inserts/inserts_nominadas.sql
@@ -1,55 +1,51 @@
 -- INI NOMINADAS
 INSERT INTO public.nominadas(
-	id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-	VALUES (1, 'no', 1, 1985, 18);
+	id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+	VALUES (1, 'no', 1, 1985, 18, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (2, 'no', 2, 1985, 19);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (2, 'no', 2, 1985, 19, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (3, 'no', 3, 1985, 20);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (3, 'no', 3, 1985, 20, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (4, 'no', 4, 1985, 21);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (4, 'no', 4, 1985, 21, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (5, 'no', 5, 1985, 19);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (5, 'no', 5, 1985, 19, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (6, 'no', 6, 1985, 22);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (6, 'no', 6, 1985, 22, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (7, 'no', 7, 1985, 24);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (7, 'no', 7, 1985, 24, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (8, 'no', 8, 1985, 27);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (8, 'no', 8, 1985, 27, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (9, 'no', 9, 1985, 18);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (9, 'no', 9, 1985, 18, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (10, 'si', 11, 1986, 27);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (10, 'si', 11, 1986, 27, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (11, 'no', 20, 1985, 21);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (12, 'si', 12, 1986, 28, 'no', 0);
 
 INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (12, 'si', 12, 1986, 28);
-
-INSERT INTO public.nominadas(
-    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria)
-    VALUES (13, 'no', 21, 1986, 21);
+    id_nominada, ganador, id_postuladas_p_pers, ano_oscar, id_categoria, empate, ronda_emp)
+    VALUES (13, 'no', 21, 1986, 21, 'no', 0);
 
 
 -- FIN NOMINADAS
