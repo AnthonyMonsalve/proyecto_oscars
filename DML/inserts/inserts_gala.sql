9fd28541a3fc8205cd6eb08180fd6eefc1181d38
Rayner Betancourt <raynerb3000@gmail.com>
2022-07-31 13:47:33 -0400

    inserts de critica de 1987 y las dos galas que faltan

--- DML/inserts/inserts_gala.sql
+++ DML/inserts/inserts_gala.sql
@@ -10,4 +10,13 @@
     INSERT INTO public.gala(
         ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
         VALUES (1987, '1987-04-20', 'Traki del recreo', 52, 3548, 'Viene a tocar coldplay');
+
+    INSERT INTO public.gala(
+        ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
+        VALUES (1988, '1988-04-20', 'Traki del recreo', 53, 3548, 'Espectaculo de primera');
+
+    INSERT INTO public.gala(
+        ano, fecha, lugar, numero_edicion, doc_identidad, descripcion)
+        VALUES (1989, '1989-04-20', 'Traki del recreo', 54, 3548, 'Grandioso homenaje al cine');
+        
 -- FIN GALA
