708bb7f72db608072c27a4b99254a0bdd9f27a96
Rayner Betancourt <raynerb3000@gmail.com>
2022-08-01 12:34:44 -0400

    insert votos para data mart

--- votos.sql
+++ votos.sql
@@ -1,56 +1,65 @@
 ------  AÑ0 1986 ------
 
     ------  POSTULAR PELICULA   -----
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
-        postular_pelicula(1986, miem, aud);
+        postular_pelicula(1986, 1, 155);
+        postular_pelicula(1986, 2, 255);
+        postular_pelicula(1986, 3, 388);
+        postular_pelicula(1986, 4, 788);
+        postular_pelicula(1986, 5, 118);
 
     ----- GESTIONAR POSTULADOS -----
         gestion_postulados(1986);
 
     ----- VOTOS POSTULADOS ------ 
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
+        -- VOTOS ACTOR
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 5);
+            votar_postulados(id_postu, 5);
+            votar_postulados(id_postu, 6);
+            votar_postulados(id_postu, 6);
+            votar_postulados(id_postu, 7);
+            votar_postulados(id_postu, 7);
+        -- VOTOS DIRECTOR
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 5);
+            votar_postulados(id_postu, 5);
+            votar_postulados(id_postu, 6);
+            votar_postulados(id_postu, 6);
+            votar_postulados(id_postu, 7);
+            votar_postulados(id_postu, 7);
 
     ----- GESTIONAR NOMINADOS -----
         gestion_nominados(1986, 18);
         gestion_nominados(1986, 19);
 
     -----   VOTOS NOMINADO  -------
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
+        -- VOTAR ACTOR
+            votar_nominados(id_nomi %, 1);
+            votar_nominados(id_nomi %, 2);
+            votar_nominados(id_nomi %, 3);
+            votar_nominados(id_nomi /, 4);
+            votar_nominados(id_nomi /, 5);
+        -- VOTAR DIRECTOR
+            votar_nominados(id_nomi /, 1);
+            votar_nominados(id_nomi /, 2);
+            votar_nominados(id_nomi /, 3);
+            votar_nominados(id_nomi /, 4);
+            votar_nominados(id_nomi %, 5);
+            votar_nominados(id_nomi %, 6);
 
     ----- GESTION GANADORES ------ 
         gestion_ganadores(1986,18);
@@ -64,56 +73,55 @@
 ------  AÑ0 1987 ------
 
     ------  POSTULAR PELICULA   -----
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
-        postular_pelicula(1987, miem, aud);
+        postular_pelicula(1987, 1, 300);
+        postular_pelicula(1987, 2, 301);
+        postular_pelicula(1987, 3, 302);
+        postular_pelicula(1987, 4, 303);
+        postular_pelicula(1987, 2, 304);
+        postular_pelicula(1987, 4, 305);
+        postular_pelicula(1987, 1, 306);
+        postular_pelicula(1987, miem_nuevo, 307);
 
     ----- GESTIONAR POSTULADOS -----
         gestion_postulados(1987);
 
     ----- VOTOS POSTULADOS ------ 
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
+        ----- VOTAR ACTOR -----
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 1);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, miem_nuevo);
+            votar_postulados(id_postu, miem_nuevo);
+        ----- VOTAR DIRECTOR -----
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 2);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 4);
+            votar_postulados(id_postu, 3);
+            votar_postulados(id_postu, miem_nuevo);
+            votar_postulados(id_postu, miem_nuevo);
 
     ----- GESTIONAR NOMINADOS -----
         gestion_nominados(1987, 18);
         gestion_nominados(1987, 19);
 
     -----   VOTOS NOMINADO  -------
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
+        ----- VOTAR ACTOR
+            votar_nominados(id_nomi, 1);
+            votar_nominados(id_nomi, 3);
+            votar_nominados(id_nomi, 5);
+            votar_nominados(id_nomi, 2);
+            votar_nominados(id_nomi, miem_nuevo);
+        ----- VOTAR DIRECTOR -----
+            votar_nominados(id_nomi, 1);
+            votar_nominados(id_nomi, 2);
+            votar_nominados(id_nomi, 3);
+            votar_nominados(id_nomi, 4);
+            votar_nominados(id_nomi, miem_nuevo);
 
     ----- GESTION GANADORES ------ 
         gestion_ganadores(1987,18);
@@ -127,56 +135,55 @@
 ------  AÑ0 1988 ------
 
     ------  POSTULAR PELICULA   -----
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
-        postular_pelicula(1988, miem, aud);
+        postular_pelicula(1988, miem_nuevo_1, 200);
+        postular_pelicula(1988, miem_nuevo_2, 201);
+        postular_pelicula(1988, 1, 202);
+        postular_pelicula(1988, 2, 203);
+        postular_pelicula(1988, 3, 204);
 
     ----- GESTIONAR POSTULADOS -----
         gestion_postulados(1988);
 
     ----- VOTOS POSTULADOS ------ 
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
-        votar_postulados(id_postu, miem);
+        ---- VOTAR ACTOR -----
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+        
+        ----- VOTAR DIRECTOR -----
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
+            votar_postulados(id_postu, miem);
 
     ----- GESTIONAR NOMINADOS -----
         gestion_nominados(1988, 18);
         gestion_nominados(1988, 19);
 
     -----   VOTOS NOMINADO  -------
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
-        votar_nominados(id_nomi, miem);
+        ----- VOTAR ACTOR ------
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+        ----- VOTAR DIRECTOR
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
+            votar_nominados(id_nomi, miem);
 
     ----- GESTION GANADORES ------ 
         gestion_ganadores(1988,18);
